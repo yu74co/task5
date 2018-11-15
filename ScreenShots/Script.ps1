6af044134dccb4785be4af1a8be1ba15bd33a257
Yury Semchanka <sem.y@tut.by>
2018-11-15 13:59:02 +0300

    Adding screenshots and files

---
+++ ScreenShots/Script.ps1
@@ -0,0 +1,20 @@
+﻿$app = $OctopusParameters["Octopus.Action[Dep].Output.app"]
+Write-Host "Go to the directory $app "
+cd $app
+
+$maximumRuntimeSeconds = 45 
+$process = Start-Process  dotnet -ArgumentList 'MusicStore.dll' -PassThru
+
+Timeout 7
+curl http://localhost:5000 -UseBasicParsing
+
+try 
+{
+    $process | Wait-Process -Timeout $maximumRuntimeSeconds -ErrorAction Stop 
+    Write-Warning  'Process successfully completed within timeout.' 
+}
+catch 
+{
+    Write-Host "It's is time to kill the Process." 
+    $process | Stop-Process -Force 
+} 
